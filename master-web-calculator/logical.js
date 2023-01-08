4a580a82d909d081a6002c6281485c1ee01ce1ec
Kevin Ananda Putra <122136284+KevinAnandaP@users.noreply.github.com>
2023-01-08 22:05:14 +0700

    Calculator

---
+++ master-web-calculator/logical.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 12;
+
+console.log(a < 15 && b > 10);
+console.log(a > 15 && b > 10);
+
+console.log(a < 15 || b > 10);
+console.log(a > 15 || b > 10);
+
+console.log(!(a < 15));
+console.log(!(a < 15 && b > 10));
